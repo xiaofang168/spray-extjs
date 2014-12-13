215f46a5ab6098ed3e74b120d26a80339af43fb2
jeff <hbxffj@163.com>
2014-12-13 22:49:30 +0800

    完成了获取列表的查询实现

--- src/main/scala/com/jeff/services/ContractStatusService.scala
+++ src/main/scala/com/jeff/services/ContractStatusService.scala
@@ -1,15 +1,17 @@
 package com.jeff.services
 
 import com.jeff.entities.Tables
+import scala.slick.lifted.TableQuery
+import scala.slick.driver.MySQLDriver.simple._
 
 /**
  * 合同进程
  * Created by fangj on 2014/11/21.
  */
-trait ContractStatusService extends BaseService{
+trait ContractStatusService extends BaseService {
 
   def getById(id: Long) = {
-	 
+
   }
 
   def update() = {
@@ -22,5 +24,10 @@ trait ContractStatusService extends BaseService{
 
   def save() = "test"
 
-  def all() = "test"
+  def all(): List[Tables.ProxyRow] = {
+    db.withSession { implicit session =>
+      Tables.Proxy.list
+    }
+  }
+
 }
