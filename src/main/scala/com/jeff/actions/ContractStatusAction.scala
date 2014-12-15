80b29146e1185353823745a3310558e6ec33b6ef
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-15 12:16:22 +0800

    增加了接口参数

--- src/main/scala/com/jeff/actions/ContractStatusAction.scala
+++ src/main/scala/com/jeff/actions/ContractStatusAction.scala
@@ -1,15 +1,15 @@
 package com.jeff.actions
 
 import com.jeff.api.RequestMessage
+import com.jeff.entities.Tables
 
 /**
  * Created by fangj on 2014/11/21.
  */
 object ContractStatusAction extends CommonAction {
 
-  case class Update() extends RequestMessage
+  case class Update(id:Int, proxy: Tables.ProxyRow) extends RequestMessage
 
-  case class Save() extends RequestMessage
-
+  case class Save(proxy: Tables.ProxyRow) extends RequestMessage
 
 }
