7005ae76311e3a9c2bcebfeb8f1876aa1414bd42
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-15 12:17:46 +0800

    增加了proxy 路由（查询、添加、修改、删除）

--- src/main/scala/com/jeff/api/MyService.scala
+++ src/main/scala/com/jeff/api/MyService.scala
@@ -3,9 +3,9 @@ package com.jeff.api
 import akka.actor.{ Actor, Props }
 import com.jeff.actions.ContractStatusAction
 import com.jeff.actors.ContractStatusActor
-
 import spray.routing._
 import spray.http._
+import com.jeff.entities.Tables
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -27,6 +27,9 @@ class MyServiceActor extends Actor with MyService with CustomerRequestCreator {
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
 
+  import com.jeff.api.JsonImplicits._
+  import spray.httpx.SprayJsonSupport.sprayJsonUnmarshaller
+
   // 合同进程路由定义
   val contractStatusRoute = path("proxy") {
     get {
@@ -35,8 +38,34 @@ trait MyService extends HttpService {
           handleContractStatusRequest(ContractStatusAction.All)
         }
       }
+    } ~
+      post {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          entity(as[Tables.ProxyRow]) { proxy =>
+            handleContractStatusRequest(ContractStatusAction.Save(proxy))
+          }
+        }
+      }
+  } ~
+    path("proxy" / Segment) { (id) =>
+      get {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          handleContractStatusRequest(ContractStatusAction.Get(id.toInt))
+        }
+      }
+      put {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          entity(as[Tables.ProxyRow]) { proxy =>
+            handleContractStatusRequest(ContractStatusAction.Update(id.toInt, proxy))
+          }
+        }
+      } ~
+        delete {
+          respondWithMediaType(MediaTypes.`application/json`) {
+            handleContractStatusRequest(ContractStatusAction.Delete(id.toInt))
+          }
+        }
     }
-  }
 
   def handleContractStatusRequest(requsetMessage: RequestMessage): Route
 
