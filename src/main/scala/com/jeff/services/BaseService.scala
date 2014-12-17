89f0833a3b59646fe642d7e53ae69aad6b8b3235
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-17 20:57:28 +0800

    增加了动态查询辅助类

--- src/main/scala/com/jeff/services/BaseService.scala
+++ src/main/scala/com/jeff/services/BaseService.scala
@@ -8,14 +8,20 @@ package com.jeff.services
 
 import com.jeff.db.DBConnection
 import com.jeff.db.SlickDBDriver
-import scala.slick.lifted.TableQuery
-import com.jeff.entities.Tables
+import scala.slick.lifted.Column
 
 /**
  * @author: <a href="mailto:hbxffj@163.com">方杰</a>
  * @Date: 2014年11月29日
  * @version: $Rev$
  */
+case class MaybeFilter[X, Y](val query: scala.slick.lifted.Query[X, Y, Seq]) {
+  def filteredBy(op: Option[_])(f: (X) => Column[Option[Boolean]]) = {
+    op map { o => MaybeFilter(query.filter(f)) } getOrElse { this }
+  }
+}
+
 private[services] trait BaseService {
+  implicit def maybeFilterConversor[X, Y](q: scala.slick.lifted.Query[X, Y, Seq]) = new MaybeFilter(q)
   def db = new DBConnection(SlickDBDriver.getDriver).dbObject
 }
