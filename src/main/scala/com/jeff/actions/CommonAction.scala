bc1568b96b9e5b6764693f220ab7edcd4efb368e
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-17 20:54:03 +0800

    增加了通用的查询方法

--- src/main/scala/com/jeff/actions/CommonAction.scala
+++ src/main/scala/com/jeff/actions/CommonAction.scala
@@ -2,6 +2,8 @@ package com.jeff.actions
 
 import com.jeff.api.RequestMessage
 
+import com.jeff.entities.Query._
+
 /**
  * Created by fangj on 2014/11/21.
  */
@@ -12,5 +14,7 @@ private[actions] trait CommonAction {
   case class Get(id: Int) extends RequestMessage
 
   case object All extends RequestMessage
+
+  case class Search($offset: Option[Int] = Some(0), $limit: Option[Int] = Some(20), $sort: Option[Array[Sort]] = Some(Array[Sort](Sort("id", Order.DESC))), $filter: Option[Filter])
 
 }
