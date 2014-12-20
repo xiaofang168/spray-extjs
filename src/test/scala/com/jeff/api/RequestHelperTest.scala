35be93c0995c66ed13fcd23cb727a7191c2f717e
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-20 18:52:40 +0800

    增加了根据url参数获取查询对象单元测试类

---
+++ src/test/scala/com/jeff/api/RequestHelperTest.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: spray-extjs
+ * Description: RequestHelperTest.scala
+ * created at: 2014年12月20日
+ */
+package com.jeff.api
+
+import org.junit.Test
+import com.jeff.entities.Query
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年12月20日 上午11:23:25
+ * @version: $Rev$
+ */
+class RequestHelperTest {
+
+  @Test
+  def testGetSearch() {
+    val $sort = """[{"property":"id","direction":"DESC"}]"""
+    val $filter = """[{"property":"id","value":1,"expression":"="},{"property":"$or","value":[{"property":"age","value":10,"expression":"<"},{"property":"age","value":20,"expression":">"}]}]"""
+    val params = Map("$page" -> "1", "$offset" -> "10", "$limit" -> "5", "$sort" -> $sort, "$filter" -> $filter)
+    RequestHelper.getSearch(params)
+  }
+  
+  
+  @Test
+  def testE() {
+    println(Query.Order.DESC.toString())
+  }
+
+}
