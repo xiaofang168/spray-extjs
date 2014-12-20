b0a479cf6f727051a996b300eed3a70101957334
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-20 18:50:31 +0800

    增加了进口合同进程管理服务的路由设置

---
+++ src/main/scala/com/jeff/api/ExportContractProgressServiceRoute.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: spray-extjs
+ * Description: ExportContractProgressRoute.scala
+ * created at: 2014年12月20日
+ */
+package com.jeff.api
+
+/**
+ * 出口合同进程服务路由
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年12月20日 上午11:02:43
+ * @version: $Rev$
+ */
+class ExportContractProgressServiceRoute {
+
+}
