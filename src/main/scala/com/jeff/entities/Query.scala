44035ae547ced6cc147e1ecc170f427d51955403
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-20 18:51:00 +0800

    增加了Search查询模式类

--- src/main/scala/com/jeff/entities/Query.scala
+++ src/main/scala/com/jeff/entities/Query.scala
@@ -20,11 +20,23 @@ object Query extends {
 trait Query {
   case class Sort(property: String, direction: Order.Value)
   case class Filter(property: String, value: Any, expression: Option[Expression.Value])
+  case class Search(offset: Option[Int] = Some(0), limit: Option[Int] = Some(20), sort: Option[Array[Sort]] = Some(Array[Sort](Sort("id", Order.DESC))), filter: Option[Array[Filter]])
   object Order extends Enumeration {
     val ASC, DESC = Value
   }
   object Expression extends Enumeration {
     val EQ, /**不等于*/ NEQ, /**小于*/ LT, /**小于或等于*/ LE, /**大于*/ GT, /**大于或等于*/ GE, /**in*/ IN, /**not in*/ NIN, /**between*/ BETWEEN, /**null*/ NULL, /**like*/ LIKE, /**or*/ OR, /**and*/ AND = Value
   }
+
+  /**请求参数*/
+  object RequestParams {
+    val page = "$page"
+    val offset = "$offset"
+    val limit = "$limit"
+    val sort = "$sort"
+    val filter = "$filter"
+  }
+
 }
+
 
