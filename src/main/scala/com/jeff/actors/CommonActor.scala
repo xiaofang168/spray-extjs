83f93dae83f268b0156a22a73a6225614f9f47f5
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-24 18:39:39 +0800

    增加了CommonActor处理类，处理导出业务

---
+++ src/main/scala/com/jeff/actors/CommonActor.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: spray-extjs
+ * Description: CommonActor.scala
+ * created at: 2014年12月24日
+ */
+package com.jeff.actors
+
+import akka.actor.Actor
+import com.jeff.actions.CommonAction
+import com.jeff.actions.ContractStatusAction
+import com.jeff.services.ContractStatusService
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年12月24日 下午6:23:30
+ * @version: $Rev$
+ */
+class CommonActor extends Actor {
+  def receive = {
+    case ContractStatusAction.Export => sender ! new ContractStatusService {}.export
+  }
+}
