f53fea456bdfd1602b9bdc92bfc51c968fb021ba
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-26 14:55:53 +0800

    支持多个id值删除（split with ","）

--- src/main/scala/com/jeff/actions/CommonAction.scala
+++ src/main/scala/com/jeff/actions/CommonAction.scala
@@ -10,6 +10,8 @@ import com.jeff.entities.Query._
 private[actions] trait CommonAction {
 
   case class Delete(id: Int) extends RequestMessage
+  
+  case class Deletes(ids: Array[Int]) extends RequestMessage
 
   case class Get(id: Int) extends RequestMessage
 
