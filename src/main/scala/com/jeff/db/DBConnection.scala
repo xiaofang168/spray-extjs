fa7450abc29f84798b581f2e7ef1cef5bf661c8b
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-12-26 15:04:00 +0800

    println 输出改为 logback 输出

--- src/main/scala/com/jeff/db/DBConnection.scala
+++ src/main/scala/com/jeff/db/DBConnection.scala
@@ -2,11 +2,15 @@ package com.jeff.db
 
 import scala.slick.driver.JdbcProfile
 import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
 /**
  * Created by fangj on 2014/11/22.
  */
 class DBConnection(override val profile: JdbcProfile) extends Profile {
+
+  def logger = LoggerFactory.getLogger(classOf[DBConnection])
+
   import profile.simple._
   def dbObject(): Database = {
     val env = scala.util.Properties.envOrElse("runMode", "dev")
@@ -15,7 +19,7 @@ class DBConnection(override val profile: JdbcProfile) extends Profile {
     val username = config.getString("db.username")
     val password = config.getString("db.password")
     val driver = config.getString("db.driver")
-    println("Connection info =>" + "Run mode: " + env + ", db url: " + url + ", driver: " + driver)
+    logger.info("Connection info =>" + "Run mode: " + env + ", db url: " + url + ", driver: " + driver)
     Database.forURL(url, username, password, null, driver)
   }
 }
